7c4b71ae3c2869c76bce38d6d27619c3cb4068aa
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2024-09-19 20:01:36 +0530

    "Updated backend: modified package.json, added constants.js, and refactored index.js MongoDB Connect"

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,26 @@
-console.log("Hello via Bun!");
+// require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+// const app = express();
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("ERROR: ", error);
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is Listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("ERROR: ", error);
+//     throw error;
+//   }
+// })();
